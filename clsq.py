2f52d52e482f5b88530d6abd2b05c6179b487bc2
bjw9808 <bjw9808@gmail.com>
2020-04-04 18:22:34 +0800

    开了五十个进程，直接拉满CPU，舒服

--- clsq.py
+++ clsq.py
@@ -1,26 +1,28 @@
 from selenium import webdriver
 import requests
 import time
-import os
 from multiprocessing import Process
+import myLog
+import sys
+from multiprocessing import Pool
 
 proxies = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080"}
 
-def download_pic(url, pic_title):
+def download_pic(url, pic_title, log_instance):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36 Edg/80.0.361.109'
     }
-    retry_count = 1;
+    retry_count = 1
     pic_title = pic_title + '.jpg'
     while retry_count < 11:
         try:
             resp = requests.get(url = url, proxies=proxies, headers = headers, timeout = 30)
             with open('E:/pic/' + pic_title, 'wb') as f:
                 f.write(resp.content)
-            print("file " + pic_title + "done")
+            log_instance.write_log(get_runtime_msg() + "file " + pic_title + "done")
             break
         except:
-            print("file " + pic_title + " download error, retry " + str(retry_count) + " times")
+            log_instance.write_log(get_runtime_msg('error') + "file " + pic_title + " download error, retry " + str(retry_count) + " times")
             time.sleep(1)
             continue
 
@@ -29,8 +31,17 @@ def filename_replace(filename):
         filename = filename.replace(str_illegal, '-')
     return filename
 
+def get_runtime_msg(flag = ''):
+    if flag == '':
+        msg = "[" + sys._getframe().f_code.co_filename + str(sys._getframe().f_lineno) + "] [DEBUG] "
+        return msg
+    elif flag == 'error':
+        msg = "[" + sys._getframe().f_code.co_filename + str(sys._getframe().f_lineno) + "] [ERROR] "
+        return msg
+
 def start(start_page ,pid_target):
-    print("process start, NO." + str(start_page) + " !")
+    log_instance = myLog.my_Log()
+    log_instance.write_log(get_runtime_msg() + "process start, NO." + str(start_page) + " !")
     # 设置本地代理
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
@@ -49,53 +60,36 @@ def start(start_page ,pid_target):
     for i in range(start_page, 100, pid_target):
         while True:
             try:
-                print("getting: NO. " + str(i) + " page")
+                log_instance.write_log(get_runtime_msg() + "getting: NO. " + str(i) + " page")
                 browser.get("http://t66y.com/thread0806.php?fid=8&search=&page=" + str(i))
-                print("NO." + str(i) + " page load successful, url: " + "http://t66y.com/thread0806.php?fid=8&search=&page=" + str(i))
+                log_instance.write_log(get_runtime_msg() + "NO." + str(i) + " page load successful, url: " + "http://t66y.com/thread0806.php?fid=8&search=&page=" + str(i))
                 break
             except:
                 time.sleep(3)
-                print("get url " + "http://t66y.com/thread0806.php?fid=8&search=&page=" + str(i) + " failed,try again")
+                log_instance.write_log(get_runtime_msg('error') + "get url " + "http://t66y.com/thread0806.php?fid=8&search=&page=" + str(i) + " failed,try again")
                 continue
         for item in xpath_list:
             try:
                 browser.find_element_by_xpath(item).click()
                 n = browser.window_handles
                 browser.switch_to.window(n[-1])
-                print("getting url: " + str(browser.current_url) + " start...")
+                log_instance.write_log(get_runtime_msg() + "getting url: " + str(browser.current_url) + " start...")
                 img_list = browser.find_elements_by_tag_name("img")
                 pic_title = filename_replace(browser.title)
                 for i in range(len(img_list)):
-                    download_pic(img_list[i].get_attribute('src'), pic_title + '(' + str(i) + ')')
+                    download_pic(img_list[i].get_attribute('src'), pic_title + '(' + str(i) + ')', log_instance)
                 browser.close()
                 browser.switch_to.window(n[0])
-            except:
+            except Exception as error:
+                log_instance.write_log(get_runtime_msg('error') + str(error))
                 browser.quit()
                 continue
         browser.quit()
 
 if __name__ == '__main__':
-    p_1 = Process(target=start, args=(1, 8))
-    p_2 = Process(target=start, args=(2, 8))
-    p_3 = Process(target=start, args=(3, 8))
-    p_4 = Process(target=start, args=(4, 8))
-    p_5 = Process(target=start, args=(5, 8))
-    p_6 = Process(target=start, args=(6, 8))
-    p_7 = Process(target=start, args=(7, 8))
-    p_8 = Process(target=start, args=(8, 8))
-    p_1.start()
-    p_2.start()
-    p_3.start()
-    p_4.start()
-    p_5.start()
-    p_6.start()
-    p_7.start()
-    p_8.start()
-    p_1.join()
-    p_2.join()
-    p_3.join()
-    p_4.join()
-    p_5.join()
-    p_6.join()
-    p_7.join()
-    p_8.join()
+    pool_nums = 50
+    clsq_pool = Pool(pool_nums)
+    for i in range(1, pool_nums + 1):
+        clsq_pool.apply_async(start, args=(i,pool_nums + 1))
+    clsq_pool.close()
+    clsq_pool.join()
